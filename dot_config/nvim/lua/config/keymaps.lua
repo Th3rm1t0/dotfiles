328c1d85d7fa24f4303b9e83c1918503b1023bbb
Th3rm1t0 <80260010+Th3rm1t0@users.noreply.github.com>
2025-07-15 01:12:14 +0900

    Update .config/nvim/lua/config/keymaps.lua

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,21 +1,55 @@
--- キーマッピング
-local keymap = vim.keymap
+-- =============================================================================
+-- Key Mappings Configuration
+-- =============================================================================
 
--- 基本的なキーマッピング
-keymap.set("n", "pv", vim.cmd.Ex)
-keymap.set("v", "J", ":m '>+1gv=gv")
-keymap.set("v", "K", ":m '<-2gv=gv")
-keymap.set("n", "J", "mzJ`z")
-keymap.set("n", "", "zz")
-keymap.set("n", "", "zz")
-keymap.set("n", "n", "nzzzv")
-keymap.set("n", "N", "Nzzzv")
+local function map(mode, lhs, rhs, opts)
+    local options = { noremap = true, silent = true }
+    if opts then
+        options = vim.tbl_extend("force", options, opts)
+    end
+    vim.keymap.set(mode, lhs, rhs, options)
+end
 
--- コピー/ペースト
-keymap.set("x", "p", [["_dP]])
-keymap.set({"n", "v"}, "y", [["+y]])
-keymap.set("n", "Y", [["+Y]])
-keymap.set({"n", "v"}, "d", [["_d]])
+-- Leader key
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
--- 検索と置換
-keymap.set("n", "s", [[:%s/\<\>//gI]])
+-- =============================================================================
+-- General Mappings
+-- =============================================================================
+
+-- File operations
+map("n", "<leader>w", ":w<CR>", { desc = "Save file" })
+map("n", "<leader>q", ":q<CR>", { desc = "Quit" })
+map("n", "<leader>x", ":x<CR>", { desc = "Save and quit" })
+
+-- Window navigation
+map("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
+map("n", "<C-j>", "<C-w>j", { desc = "Move to bottom window" })
+map("n", "<C-k>", "<C-w>k", { desc = "Move to top window" })
+map("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
+
+-- Buffer navigation
+map("n", "<S-h>", ":bprevious<CR>", { desc = "Previous buffer" })
+map("n", "<S-l>", ":bnext<CR>", { desc = "Next buffer" })
+map("n", "<leader>bd", ":bdelete<CR>", { desc = "Delete buffer" })
+
+-- Search
+map("n", "<leader>hl", ":nohl<CR>", { desc = "Clear search highlight" })
+
+-- Insert mode navigation
+map("i", "<C-h>", "<Left>", { desc = "Move left" })
+map("i", "<C-j>", "<Down>", { desc = "Move down" })
+map("i", "<C-k>", "<Up>", { desc = "Move up" })
+map("i", "<C-l>", "<Right>", { desc = "Move right" })
+
+-- Visual mode indentation
+map("v", "<", "<gv", { desc = "Indent left" })
+map("v", ">", ">gv", { desc = "Indent right" })
+
+-- Line movement
+map("n", "<A-j>", ":m .+1<CR>==", { desc = "Move line down" })
+map("n", "<A-k>", ":m .-2<CR>==", { desc = "Move line up" })
+map("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
+map("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
+
