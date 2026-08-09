5295f475435a68a32656cf8d1a569b10a15fb72b
Th3rm1t0 <80260010+Th3rm1t0@users.noreply.github.com>
2026-08-09 13:56:33 +0000

    バーとランチャーを追加

--- hosts/roles/desktop.nix
+++ hosts/roles/desktop.nix
@@ -2,20 +2,41 @@
 {
   programs.foot.enable = true;
 
+  programs.rofi = {
+    enable = true;
+    package = pkgs.rofi-wayland;
+  };
+
+  programs.waybar = {
+    enable = true;
+    settings.mainBar = {
+      layer = "top";
+      position = "top";
+      height = 32;
+      modules-left = [ "hyprland/workspaces" ];
+      modules-center = [ "clock" ];
+      modules-right = [ "pulseaudio" "network" "battery" "tray" ];
+    };
+  };
+
   wayland.windowManager.hyprland = {
     enable = true;
     settings = {
       "$mod" = "SUPER";
       bind = [
-        "$mod, Return, exec, foot"   # 仮のターミナル
+        "$mod, Return, exec, foot"
         "$mod, Q, killactive"
         "$mod SHIFT, E, exit"
+        "$mod, V, togglefloating"
+        "$mod, D, exec, rofi -show drun"
       ];
-      input = {
-        kb_layout = "jp";
-      };
+      bindm = [
+        "$mod, mouse:272, movewindow"
+        "$mod, mouse:273, resizewindow"
+      ];
       exec-once = [
         "${pkgs.kdePackages.polkit-kde-agent-1}/libexec/polkit-kde-authentication-agent-1"
+        "waybar"
       ];
     };
   };
