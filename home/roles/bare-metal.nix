9c63d2a1de78e4a783d0c02e7b3ffc49479ee89a
Th3rm1t0 <80260010+Th3rm1t0@users.noreply.github.com>
2026-08-17 04:31:59 +0900

    1Password SSH エージェントによる commit 署名を有効化
    gpg.ssh.program を lib.getExe' でのパス解決に変更し、
    user.signingKey と gpg.ssh.allowedSignersFile を設定する。

--- home/roles/bare-metal.nix
+++ home/roles/bare-metal.nix
@@ -1,12 +1,19 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 {
   home.sessionVariables.SSH_AUTH_SOCK = "${config.home.homeDirectory}/.1password/agent.sock";
+
+  xdg.configFile."git/allowed_signers".text =
+    "80260010+Th3rm1t0@users.noreply.github.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPit0XMIE0xrI2ZdX2R94TbvDE+B4V1bEUb18HAAAQQA GitHub\n";
 
   programs.git.settings = {
     gpg = {
       format = "ssh";
-      ssh.program = "${pkgs._1password-gui}/share/1password/op-ssh-sign";
+      ssh = {
+        program = "${lib.getExe' pkgs._1password-gui "op-ssh-sign"}";
+        allowedSignersFile = "${config.xdg.configHome}/git/allowed_signers";
+      };
     };
+    user.signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPit0XMIE0xrI2ZdX2R94TbvDE+B4V1bEUb18HAAAQQA GitHub";
     commit.gpgsign = true;
   };
 }
