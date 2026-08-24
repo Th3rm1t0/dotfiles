5f2fad76b2a629f52279e7d65aeaf4ae2111dd64
Th3rm1t0 <80260010+Th3rm1t0@users.noreply.github.com>
2026-08-25 01:52:55 +0900

    dapui の disconnect リスナーを追加
    terminate() 時に terminated/exited を送らず disconnect のみ
    返すアダプタがあるため、そちらでも閉じるようにする。

--- home/programs/neovim/lua/plugins/dap.lua
+++ home/programs/neovim/lua/plugins/dap.lua
@@ -48,6 +48,8 @@ return {
             dap.listeners.after.event_initialized["dapui_config"] = function() dapui.open() end
             dap.listeners.before.event_terminated["dapui_config"] = function() dapui.close() end
             dap.listeners.before.event_exited["dapui_config"] = function() dapui.close() end
+            -- terminate() は disconnect しか来ないアダプタがある
+            dap.listeners.before.disconnect["dapui_config"] = function() dapui.close() end
         end,
     },
     {
